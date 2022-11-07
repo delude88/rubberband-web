876c5a89981188f95d84f7c9f82427d368fb92fa
delude88 <50399170+delude88@users.noreply.github.com>
2022-11-07 18:24:44 +0000

    Deploying to gh-pages from @ delude88/rubberband-web@6fa8e7f1fb8c9c67f2ab28013b4ab30dc76a333e 🚀

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {
   createPitchShiftWorker,
   PitchShiftSourceNode
 } from './../..'
+import * as createModule from "../../wasm/build/rubberband.js"
 
 const audioContext = new AudioContext()
 const worker = createPitchShiftWorker('../../public/pitch-shift.worker.js')
@@ -63,7 +64,7 @@ class Core {
   setPlayBuffer = async (audioBuffer: AudioBuffer) => {
     console.info('setPlayBuffer')
     this.playBuffer = audioBuffer
-    if(this.onLoaded) {
+    if (this.onLoaded) {
       this.onLoaded()
     }
     if (this.playing) {
@@ -99,7 +100,7 @@ class Core {
         await this.setPlayBuffer(this.source)
       }
     } else {
-      if(this.onUnloaded) {
+      if (this.onUnloaded) {
         this.onUnloaded()
       }
     }
@@ -121,11 +122,11 @@ const tempoChooser = document.getElementById('tempo') as HTMLInputElement
 const core = new Core()
 
 core.onStarted = () => {
-  playButton.textContent = "Stop"
+  playButton.textContent = 'Stop'
 }
 
 core.onStopped = () => {
-  playButton.textContent = "Play"
+  playButton.textContent = 'Play'
 }
 
 core.onLoaded = () => {
@@ -173,3 +174,50 @@ fileChooser.onchange = () => {
 }
 
 playButton.disabled = true
+
+const testButton = document.getElementById('test') as HTMLButtonElement
+testButton.onclick = async () => {
+  const module = await createModule()
+  console.log(module)
+  const size = 29279232
+  const channelCount = 2
+
+  const firstPtr = module._malloc(channelCount * size * Float32Array.BYTES_PER_ELEMENT);
+  const firstTransport = new Uint8Array(module.HEAPU8.buffer, firstPtr, channelCount * size * Float32Array.BYTES_PER_ELEMENT)
+  console.info(firstTransport.byteLength)
+
+  const secondPtr = module._malloc(channelCount * size * Float32Array.BYTES_PER_ELEMENT);
+  const secondTransport = new Float32Array(module.HEAPF32.buffer, secondPtr, channelCount * size)
+  console.info(secondTransport.byteLength)
+
+  console.info(firstTransport.byteLength, secondTransport.byteLength)
+
+  console.info("Creating input")
+  const input: Float32Array[] = []
+  for(let c = 0; c < channelCount; ++c) {
+    const inputChannel = new Float32Array(size)
+    for(let i = 0; i < size; ++i) {
+      inputChannel[i] = i + ((c + 1) * 0.1)
+    }
+    input.push(inputChannel)
+  }
+
+  console.info("Assign input to first module")
+
+
+
+
+  const outputChannel1 = new Uint8Array(module.HEAPU8.buffer, firstPtr, size * Float32Array.BYTES_PER_ELEMENT)
+  const outputChannel2 = new Uint8Array(module.HEAPU8.buffer, firstPtr, size * Float32Array.BYTES_PER_ELEMENT)
+  const outputChannel4 = new Float32Array(module.HEAPF32.buffer, secondPtr, size)
+
+  console.info("Assign input channels to first buffer")
+  outputChannel1.set(input)
+  outputChannel2.set(inputChannel2)
+  outputChannel3.set(inputChannel1)
+  outputChannel4.set(inputChannel2)
+
+  module._free(firstPtr)
+  module._free(secondPtr)
+
+}
